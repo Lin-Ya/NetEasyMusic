160a332ee508220bf03ff5fb5f67bef916f03224
Lin-Ya <fenghuiwing@outlook.com>
2018-04-26 13:31:16 +0800

    更新推荐歌单引入方式

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,64 @@
 $(function () {
+    function log(s) {
+        return console.log(s)
+    }
+    //获取最新音乐
     $.get('../get/newsong.json').then(function (res) {
-        let newsong = res;
-        console.log(res);
         if (res.code === 200) {
             loadNewsong(res.result);
         } else {
             alert('网络异常，无法获取数据，请调试网络环境')
         }
     })
+    //获取推荐歌单
+    $.get('../get/personalized.json').then(function (res) {
+        if (res.code === 200) {
+            loadPersonlized(res.result);
+        } else {
+            alert('网络异常，无法获取数据，请调试网络环境')
+        }
+    })
+
+    function loadPersonlized(result) {
+        let songLists = [];
+        for(let i=0 ;i<6; i++){
+            songLists[i] = result[i];
+        }
+        log(songLists)
+        songLists.map((list)=>{
+            let count = '';
+            count = Math.ceil((list.playCount/10000).toFixed(2));
+            let danwei = '';
+        
+            if (count.toString().length>5){
+                count = (count / 10000).toFixed(1);
+                danwei = '亿'
+            }else {
+                danwei = '万'
+            }
+            
+            let $li = $(`
+                <li>
+                    <a href="?id="${list.id}">
+                        <img src="${list.picUrl}" class="songlistCove">
+                            <p class="songlistName">${list.name}</p>
+                        <div class="colletCount">${count}${danwei}</div>
+                    </a>
+                </li>
+            `)
+            $li.appendTo('.remd-ol');
+            $('.remd-ol .loadingGif').remove();
+        })
+    }
+
+
+
+
 
     function loadNewsong(result) {
         result.map(function (obj) {
             let song = obj.song;
-            console.log(song)
             let artists = song.artists;
-
             let singer = "";
             if (artists.length > 1) {
                 artists.map((s) => {
@@ -25,7 +69,6 @@ $(function () {
             } else {
                 singer = artists[0].name;
             }
-
             let $li = $(`
                 <li>
                     <a href="/song.html?id=${song.id}" class="goplaysong">
@@ -42,10 +85,8 @@ $(function () {
                     </a >
                 </li >
             `);
-
             $li.appendTo('.remd-songlist');
-            $('.loadingGif').remove();
-
+            $('.remd-songlist .loadingGif').remove();
         })
     }
 

93c2b8d863ccf16c7df02c867d34a3cdb8899bb5
Lin-Ya <fenghuiwing@outlook.com>
2018-05-09 17:06:51 +0800

    toast

---
+++ src/js/toast.js
@@ -0,0 +1,28 @@
+//使用方法： toast(msg,time)
+
+function toast(msg, time) {
+    this.message = msg;
+    this.delay = time || 1500;
+    this.createTost();
+    this.showToast();
+}
+
+toast.prototype = {
+    createTost: function () {
+        //创建jQuery模板
+        this.$temp = $(`<div class="toast">${this.message}</div>`)
+        $('body').append(this.$temp)
+    },
+    showToast: function () {
+        let _this = this;
+        this.$temp.fadeIn(300, function () {
+            setTimeout(function () {
+                $('.toast').remove()
+            }, _this.delay)
+        })
+    }
+}
+
+function Toast(msg, time) {
+    return new toast(msg, time);
+}
